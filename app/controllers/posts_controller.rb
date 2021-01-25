343f37091f7a8736d07fea3f211ddbe991c18b1f
taiga-kishimoto-2233 <taiga.kishimoto.rf@gmail.com>
2021-01-25 15:38:04 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,11 @@
 class PostsController < ApplicationController
   def index #indexアクションを定義した
-    @posts = Post.all #すべてのレコードを@postに代入
-  end
-
-  def new
+    @posts = Post.all.order(id: "DESC") #すべてのレコードを@postに代入、メモを降順で表示
   end
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
 
 end
